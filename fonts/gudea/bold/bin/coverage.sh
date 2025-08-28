0f0bae4e919731c5b9d7b26203cd974c16bdb810
Steve Tinney <stinney@upenn.edu>
2025-08-28 08:12:19 -0400

    gudea bold subset needs less than 40 chars

--- fonts/gudea/bold/bin/coverage.sh
+++ fonts/gudea/bold/bin/coverage.sh
@@ -2,4 +2,4 @@
 jx.sh Gudea-Bold.vfj >gb.xml
 xsltproc ~/santak/tools/vfj/vfj-chars.xsl gb.xml >gb.chr
 grep -F -v . gb.chr | cut -f2 | grep -vf - gudea.rep >gb.not
-wc -l gudea.rep gb.chr gb.not
+wc -l gudea.rep gb.chr gb.not | grep -v total
